e3ff402579d277ccc7c1b2b021ba4993814fd1d6
Riceball LEE <snowyu.lee@gmail.com>
2014-12-01 01:46:42 +0800

    * bug iterator-recursion-test.js max stack exceed.

--- testCommon.js
+++ testCommon.js
@@ -4,7 +4,8 @@ var path      = require('path')
 
 //let debug more easy.
 global.setImmediate = function(callback) {
-  callback()
+  setTimeout(callback, 0) //for iterator-recursion-test.js
+  //callback() //will raise max stack exceed error!
 }
 
 var dbidx = 0
