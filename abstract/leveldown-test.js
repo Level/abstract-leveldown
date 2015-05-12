e6d1cb80eaa3f4c66e43c46acd1fb90d800d6b60
Lars-Magnus Skog <ralphtheninja@riseup.net>
2015-05-12 14:16:20 +0200

    .throws is different for tape

--- abstract/leveldown-test.js
+++ abstract/leveldown-test.js
@@ -2,17 +2,18 @@ module.exports.args = function (leveldown, test) {
   test('test database creation no-arg throws', function (t) {
     t.throws(
         leveldown
-      , { name: 'Error', message: 'constructor requires at least a location argument' }
+      , /constructor requires at least a location argument/
       , 'no-arg leveldown() throws'
     )
     t.end()
   })
+
   test('test database creation non-string location throws', function (t) {
     t.throws(
         function () {
           leveldown({})
         }
-      , { name: 'Error', message: 'constructor requires a location string argument' }
+      , /constructor requires a location string argument/
       , 'non-string location leveldown() throws'
     )
     t.end()
