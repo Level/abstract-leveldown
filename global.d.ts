dd7a80cb53fa86dabf4c2cc28fb229b4012fa880
Meirion Hughes <mhughes@geologging.com>
2018-05-18 10:47:03 +0100

    chore: remove typings

--- global.d.ts
+++
@@ -1,7 +0,0 @@
-// This file is required for the use of process.browser, which is
-// not defined in @types/node but a property added by browserify.
-declare namespace NodeJS {
-  interface Process {
-    browser: boolean;
-  }
-}
