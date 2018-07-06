eac7aa6080777d1fc461d42bc826d1a520d164d9
Vincent Weevers <mail@vincentweevers.nl>
2018-07-06 12:28:02 +0200

    simplify testCommon

--- testCommon.js
+++ testCommon.js
@@ -1,21 +1,13 @@
 var collectEntries = require('level-concat-iterator')
 var tempy = require('tempy')
 
-var location = function () {
-  return tempy.directory()
-}
-
-var setUp = function (t) {
-  t.end()
-}
-
-var tearDown = function (t) {
-  t.end()
-}
-
 module.exports = {
-  location: location,
-  setUp: setUp,
-  tearDown: tearDown,
-  collectEntries: collectEntries
+  location: tempy.directory,
+  collectEntries: collectEntries,
+  setUp: function (t) {
+    t.end()
+  },
+  tearDown: function (t) {
+    t.end()
+  }
 }
