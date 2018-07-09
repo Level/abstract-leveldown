2ac490d6c5451401ff57c1b2f9b727e5b109a688
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-07-09 15:47:15 +0200

    Remove missing location test

--- test/leveldown-test.js
+++ test/leveldown-test.js
@@ -1,8 +1,4 @@
 module.exports.args = function (leveldown, test) {
-  test('test database creation no-arg default location', function (t) {
-    t.equal(leveldown().location, '')
-    t.end()
-  })
   test('test database creation non-string location throws', function (t) {
     t.throws(
       function () {
@@ -17,6 +13,7 @@ module.exports.args = function (leveldown, test) {
   test('test database open no-arg throws', function (t) {
     var db = leveldown('foo')
     t.ok(db, 'database object returned')
+    t.equal(db.location, 'foo')
     t.ok(typeof db.open === 'function', 'open() function exists')
     t.end()
   })
