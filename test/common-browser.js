378477be3de466d25f3475abf834595e51c73e27
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-07-10 20:01:55 +0200

    Remove collectEntries from common{,-browser}.js, no longer needed

--- test/common-browser.js
+++ test/common-browser.js
@@ -1,12 +1,8 @@
-var collectEntries = require('level-concat-iterator')
-
 module.exports = {
   // TODO remove?
   location: function () {
     return ''
   },
-  // Remove and use level-concat-iterator explicitly where applicable?
-  collectEntries: collectEntries,
   setUp: function (t) {
     t.end()
   },
--- test/common.js
+++ test/common.js
@@ -1,11 +1,8 @@
-var collectEntries = require('level-concat-iterator')
 var tempy = require('tempy')
 
 module.exports = {
   // TODO remove?
   location: tempy.directory,
-  // Remove and use level-concat-iterator explicitly where applicable?
-  collectEntries: collectEntries,
   setUp: function (t) {
     t.end()
   },
