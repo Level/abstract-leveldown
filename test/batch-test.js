cdd435cf5e9dae6f55c5caa3a90fc01bab19ea55
Vincent Weevers <mail@vincentweevers.nl>
2018-07-14 15:06:57 +0200

    test undefined key in batch-test

--- test/batch-test.js
+++ test/batch-test.js
@@ -48,17 +48,22 @@ exports.args = function (test, testCommon) {
     async = true
   })
 
-  test('test batch() with null `key`', function (t) {
-    var async = false
+  test('test batch() with null or undefined `key`', function (t) {
+    var illegalKeys = [null, undefined]
 
-    db.batch([{ type: 'put', key: null, value: 'foo1' }], function (err) {
-      t.ok(err, 'got error')
-      t.equal(err.message, 'key cannot be `null` or `undefined`', 'correct error message')
-      t.ok(async, 'callback is asynchronous')
-      t.end()
+    t.plan(illegalKeys.length * 3)
+
+    illegalKeys.forEach(function (key) {
+      var async = false
+
+      db.batch([{ type: 'put', key: key, value: 'foo1' }], function (err) {
+        t.ok(err, 'got error')
+        t.equal(err.message, 'key cannot be `null` or `undefined`', 'correct error message')
+        t.ok(async, 'callback is asynchronous')
+      })
+
+      async = true
     })
-
-    async = true
   })
 
   test('test batch() with missing `key` and `value`', function (t) {
